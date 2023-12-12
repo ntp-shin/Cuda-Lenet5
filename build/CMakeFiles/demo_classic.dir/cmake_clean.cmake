b1c2077ad64944580aa7818175a1296a09790490
ntp-shin <tanphatnguyen2002@gmail.com>
2023-12-12 14:50:29 +0700

    1. Upload mini-dnn

---
+++ build/CMakeFiles/demo_classic.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo_classic.dir/demo_classic.cc.o"
+  "CMakeFiles/demo_classic.dir/demo_classic.cc.o.d"
+  "demo_classic"
+  "demo_classic.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo_classic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
